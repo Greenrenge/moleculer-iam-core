4edf7869e6215a09125b024545e58ebb5ddf7dd6
greenrenge <greenrenge@gmail.com>
2024-02-10 16:05:23 +0700

    only moleculer-iam

--- app-renderer/src/component/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "@ui-kitten/components";
-export * from "./util";
-export * from "./separator";
-export * from "./form";
-export * from "./persona";
-export * from "./layout";
--- app-renderer/src/index.ts
+++
@@ -1,3 +0,0 @@
-// because react-scripts doesn't support custom entry points
-// https://github.com/timarney/react-app-rewired?tab=readme-ov-file
-import("./index.tsx")
